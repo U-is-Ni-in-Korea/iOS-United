7bb270de42b7ca52f4e0d45e7e1301b62f7b6e72
parkikbum <pibum@naver.com>
2023-07-15 14:37:30 +0900

    ✨[FEAT]: homeView layout 구현
    🔨[FIX]: 버그, 오류 해결
    ➕[ADD]: Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시
    ✨[FEAT]: 새로운 기능 구현
    ✅[CHORE] : 코드 수정, 내부 파일 수정
    ⚰️[DEL] : 쓸모없는 파일,코드 삭제
    ♻️[REFACTOR] : 전면 수정이 있을 때 사용합니다
     🔀[MERGE]: 다른브렌치를 merge 할 때 사용합니다.

---
+++ Uni/Source/Scene/Home/HomeBattleView.swift
@@ -0,0 +1,8 @@
+//
+//  BattleView.swift
+//  Uni
+//
+//  Created by 박익범 on 2023/07/15.
+//
+
+import Foundation
--- Uni/Source/Scene/Home/HomeView.swift
+++ Uni/Source/Scene/Home/HomeView.swift
@@ -11,32 +11,46 @@ final class HomeView: UIView {
     init() {
         super.init(frame: .zero)
         self.setLayout()
+        self.setShadow()
     }
     
     func bindData(myScore: Int,
                   partnerScore: Int,
                   drawScore: Int,
-                  dDay: Int) {
+                  dDay: Int,
+                  heartCount: Int,
+                  isPlayingBattle: Bool) {
         self.scoreView.bindData(myScore: myScore,
                                 partnerScore: partnerScore,
                                 drawScore: drawScore,
                                 dDay: dDay)
+        self.battleView.setHeartCount(count: heartCount)
+        self.battleView.setBattleState(isPlayingBattle: isPlayingBattle)
     }
     
     private func setLayout() {
         self.backgroundColor = .white
-        self.addSubviews([topView, scoreView])
+        scoreShadowView.addSubview(scoreView)
+        self.addSubviews([topView, scoreShadowView, battleView])
         
         topView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(200)
         }
-        scoreView.snp.makeConstraints {
+        scoreShadowView.snp.makeConstraints {
             $0.top.equalTo(self.safeAreaLayoutGuide).offset(72)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(173)
         }
+        scoreView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
+        battleView.snp.makeConstraints {
+            $0.top.equalTo(scoreView.snp.bottom).offset(40)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+
         self.topView.addSubviews([logoImageView, myPageButton])
         logoImageView.snp.makeConstraints {
             $0.top.equalTo(self.safeAreaLayoutGuide).offset(15)
@@ -49,18 +63,27 @@ final class HomeView: UIView {
         }
     }
     
+    private func setShadow() {
+        scoreShadowView.layer.applyBlurAndDepth2_1Shadow()
+    }
+    
     private var topView = UIView().then {
         $0.backgroundColor = .lightBlue500
     }
-    lazy var logoImageView = UIImageView(image: .init(named: "sparkleLogo"))
+    lazy var logoImageView = UIImageView(image: SDSIcon.icLogo)
     lazy var myPageButton = UIButton().then {
+        $0.tintColor = .gray000
         var config = UIButton.Configuration.plain()
         config.background.cornerRadius = 8
         config.background.backgroundColor = .white.withAlphaComponent(0.4)
-        config.image = SDSIcon.icPerson
-//        config.image = UIImage(named: "icon")?.withTintColor(.lightBlue200)
+        config.image = SDSIcon.icPerson.withTintColor(.gray000,
+                                                      renderingMode: .alwaysTemplate)
         $0.configuration = config
     }
+    var scoreShadowView = UIView().then {
+        $0.backgroundColor = .clear
+    }
     var scoreView = ScoreView()
+    var battleView = HomeBattleView()
     
 }
