27b77879325712c43b032f581af5fd4b161566ad
parkikbum <pibum@naver.com>
2023-07-17 03:49:00 +0900

    ✨[FEAT]: battleWriteController에 loadView 추가
    🔨[FIX]: 버그, 오류 해결
    ➕[ADD]: Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시
    ✨[FEAT]: 새로운 기능 구현
    ✅[CHORE] : 코드 수정, 내부 파일 수정
    ⚰️[DEL] : 쓸모없는 파일,코드 삭제
    ♻️[REFACTOR] : 전면 수정이 있을 때 사용합니다
     🔀[MERGE]: 다른브렌치를 merge 할 때 사용합니다.

--- Uni/Source/Scene/Battle/ViewController/BattleWriteViewController.swift
+++ Uni/Source/Scene/Battle/ViewController/BattleWriteViewController.swift
@@ -2,7 +2,14 @@ import UIKit
 
 final class BattleWriteViewController: BaseViewController {
 
+    override func loadView() {
+        super.loadView()
+        self.view = battleWriteView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    private var battleWriteView = BattleWriteView()
 }
