ac26e0b80ae4b4f40ade7190f48cd2294e73338b
sarang3009 <dumpwithlove@gmail.com>
2023-07-17 00:25:09 +0900

    ➕[ADD] : 컬렉션뷰 전 UI 구현

---
+++ Uni/Source/Scene/WishCoupon/View/WishCouponView.swift
@@ -0,0 +1,8 @@
+//
+//  WishCouponView.swift
+//  Uni
+//
+//  Created by 김사랑 on 2023/07/16.
+//
+
+import Foundation
---
+++ Uni/Source/Scene/WishCoupon/ViewController/WishCouponViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WishCouponViewController.swift
+//  Uni
+//
+//  Created by 김사랑 on 2023/07/16.
+//
+
+import UIKit
+
+class WishCouponViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
