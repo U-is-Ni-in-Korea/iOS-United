e7f08d62e7d00ba5287774e177008ac4f807fd6e
parkikbum <pibum@naver.com>
2023-07-18 02:08:29 +0900

    ✨[FEAT]: 한판승부 결과 뷰 load view 삽입
    🔨[FIX]: 버그, 오류 해결
    ➕[ADD]: Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시
    ✨[FEAT]: 새로운 기능 구현
    ✅[CHORE] : 코드 수정, 내부 파일 수정
    ⚰️[DEL] : 쓸모없는 파일,코드 삭제
    ♻️[REFACTOR] : 전면 수정이 있을 때 사용합니다
     🔀[MERGE]: 다른브렌치를 merge 할 때 사용합니다.

--- Uni/Source/Scene/Battle/ViewController/BattleResultViewController.swift
+++ Uni/Source/Scene/Battle/ViewController/BattleResultViewController.swift
@@ -1,29 +1,19 @@
-//
-//  BattleResultViewController.swift
-//  Uni
-//
-//  Created by 박익범 on 2023/07/17.
-//
-
 import UIKit
+import SDSKit
+import SnapKit
+import Then
 
-class BattleResultViewController: UIViewController {
+final class BattleResultViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func loadView() {
+        super.loadView()
+        self.view = battleResultView
     }
-    */
+    
+    private let battleResultView = BattleResultView()
 
 }
