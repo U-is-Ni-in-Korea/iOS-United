2c11462777105e79fc68989c213d4a18b0533a7b
hyj422 <hyj422@icloud.com>
2023-07-17 15:28:55 +0900

    ✨[FEAT] : 설정, 계정, 프로필 수정 뷰 화면 이동 구현

---
+++ Uni/Source/Scene/Mypage/View/LogoutView.swift
@@ -0,0 +1,20 @@
+//
+//  LogoutView.swift
+//  Uni
+//
+//  Created by 홍유정 on 2023/07/17.
+//
+
+import UIKit
+
+class LogoutView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Uni/Source/Scene/Mypage/View/WithdrawView.swift
@@ -0,0 +1,20 @@
+//
+//  WithdrawView.swift
+//  Uni
+//
+//  Created by 홍유정 on 2023/07/17.
+//
+
+import UIKit
+
+class WithdrawView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
