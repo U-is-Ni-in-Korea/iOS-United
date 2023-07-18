fe7f8c469c8360159267afaae0b548215c60a368
ryuchanghwi <ckdgnl10@naver.com>
2023-07-18 18:55:45 +0900

    ✨[FEAT] : 카카오 로그인 서버 호출 메서드 구현

--- Uni/Source/Repository/AuthRepository/AuthRepository.swift
+++ Uni/Source/Repository/AuthRepository/AuthRepository.swift
@@ -3,4 +3,21 @@ import Alamofire
 
 class AuthRepository {
     
+    func postToken(token: String, completion: @escaping ((KakaoLoginDataModel) -> Void)) {
+        let params: Parameters = [
+            "code": "\(token)"
+        ]
+        PostService.shared.postService(with: params, isUseHeader: false, from: Config.baseURL + "auth/kakao", callback: {
+            (data: KakaoLoginDataModel?, error: String?) in
+            if let error = error {
+                print(error.description)
+                print("실패")
+            }
+            else if let data = data {
+                completion(data)
+                print("성공")
+                
+            }
+        })
+    }
 }
--- Uni/Source/Scene/Login/ViewController/LoginViewController.swift
+++ Uni/Source/Scene/Login/ViewController/LoginViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import KakaoSDKAuth
+import KakaoSDKUser
 
 class LoginViewController: BaseViewController {
     // MARK: - Property
     private var loginView = LoginView()
-    private lazy var kakaoAuthViewModel = KakaoAuthViewModel()
+    let authRepository = AuthRepository()
     
     // MARK: - UI Property
     
@@ -38,11 +40,59 @@ class LoginViewController: BaseViewController {
     private func actions() {
         loginView.kakaoButton.addTarget(self, action: #selector(kakaoButtonTapped), for: .touchUpInside)
     }
+    @objc func kakaoButtonTapped() {
+        hanldeKakaoLogin(completion: { kakaoToken in
+            self.authRepository.postToken(token: kakaoToken) { data in
+                print(data.accessToken, "유니토큰!!!!!!!")
+            }
+        })
+    }
     
     // MARK: - Custom Method
-    @objc func kakaoButtonTapped() {
-        kakaoAuthViewModel.hanldeKakaoLogin()
+    
+    
+    // MARK: - 카카오 로그인
+    func hanldeKakaoLogin(completion: @escaping ((String) -> Void)) {
+        print("KakaoAuthVM - handleKakaoLogin() called()")
+        //카카오톡 설치 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    if let oauthToken = oauthToken {
+                        completion(oauthToken.accessToken)
+                        print(oauthToken.accessToken)
+                    }
+                }
+            }
+        } else { //카카오톡이 설치가 안되어있으면
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                    if let error = error {
+                        print(error)
+                    }
+                    else {
+                        _ = oauthToken
+                        if let oauthToken = oauthToken {
+                            completion(oauthToken.accessToken)
+                            print(oauthToken.accessToken)
+                        }
+                    }
+                }
+        }
     }
 }
 
 // MARK: - UITableView Delegate
+//// MARK: - 로그아웃
+//func kakaoLogOut() {
+//    UserApi.shared.logout {(error) in
+//        if let error = error {
+//            print(error)
+//        }
+//        else {
+//            print("logout() success.")
+//        }
+//    }
+//}
