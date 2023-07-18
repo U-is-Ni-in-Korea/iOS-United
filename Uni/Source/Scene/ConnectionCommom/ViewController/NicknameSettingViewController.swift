35b1eb7e45681408d46fceaa836a015ff4b5e4b6
ryuchanghwi <ckdgnl10@naver.com>
2023-07-19 03:12:07 +0900

    ✨[FEAT] : 카카오 로그인 구현 및 루트 뷰 세팅 구현 완료

--- Uni/Source/Scene/ConnectionCommom/ViewController/NicknameSettingViewController.swift
+++ Uni/Source/Scene/ConnectionCommom/ViewController/NicknameSettingViewController.swift
@@ -38,15 +38,12 @@ final class NicknameSettingViewController: BaseViewController {
     // MARK: - Action Helper
     private func actions() {
         nicknameSettingView.navigationBarView.backButtonCompletionHandler = {
-//            self.keyChains.read(account: "accessToken") = {
-//                print("성공")
-//            }
             let isTokenExists = self.keyChains.isTokenExists(account: "accessToken")
             if isTokenExists {
                 print("존재")
                 self.keyChains.delete("accessToken")
+                self.navigationController?.popViewController(animated: true)
             }
-            self.navigationController?.popViewController(animated: true)
         }
     }
     
