6470f1fe62b2f6083edf7fc898962db8cab0a928
ryuchanghwi <ckdgnl10@naver.com>
2023-07-20 14:04:54 +0900

    ✨[FEAT] : 초대 코드 공유하기 기능 추가

--- Uni/Source/Scene/SendInvitation/ViewController/CodeGeneratorViewController.swift
+++ Uni/Source/Scene/SendInvitation/ViewController/CodeGeneratorViewController.swift
@@ -34,13 +34,65 @@ final class CodeGeneratorViewController: BaseViewController {
     }
     override func setConfig() {
         super.setConfig()
-        codeGeneratorView.codeLabel.text = inviteCode 
+        codeGeneratorView.codeLabel.text = inviteCode
     }
 
     // MARK: - Action Helper
     private func actions() {
+        let shareButtonTapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(shareButtonTapped))
+        shareButtonTapGesture.delegate = self
+        codeGeneratorView.shareCodeButton.addGestureRecognizer(shareButtonTapGesture)
+        let linkCheckButtonTapGesture = UITapGestureRecognizer(target: self,
+                                                               action: #selector(linkCheckButtonTapped))
+        linkCheckButtonTapGesture.delegate = self
+        codeGeneratorView.connectionCheckButton.addGestureRecognizer(linkCheckButtonTapGesture)
+        
+        codeGeneratorView.shareCodeButton.addTarget(self, action: #selector(shareButtonTapped), for: .touchUpInside)
+        
+        codeGeneratorView.connectionCheckButton.addTarget(self, action: #selector(linkCheckButtonTapped), for: .touchUpInside)
+        
+    }
+    @objc func shareButtonTapped() {
+        print("ddd")
+        
+        
+        let activityViewController = UIActivityViewController(activityItems: [inviteCode], applicationActivities: nil)
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            if let window = windowScene.windows.first {
+                window.rootViewController?.present(activityViewController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @objc func linkCheckButtonTapped() {
+        print(">>>>>>")
+        codeGeneratorView.indicatorView.startAnimating()
+        codeGeneratorView.indicatorView.isHidden = false
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            self.codeGeneratorView.indicatorView.stopAnimating()
+            self.codeGeneratorView.indicatorView.isHidden = true
+            let hasSafeArea = self.checkHomeButtonDevice()
+            self.view.showToast(message: "상대가 아직 연결되지 않았어요", hasSafeArea: hasSafeArea)
+        })
     }
 
     // MARK: - Custom Method
-  
+    func checkHomeButtonDevice() -> Bool {
+        let screenHeight = UIScreen.main.bounds.size.height
+        if screenHeight <= 736 {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+}
+
+extension CodeGeneratorViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
 }
