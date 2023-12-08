e8137bbee44b974851cc39e8355c9e65c139fd20
ryuchanghwi <ckdgnl10@naver.com>
2023-12-08 17:50:43 +0900

    ✅[CHORE] : 타이머 기능 수정

--- Uni/Source/Scene/Timer/Component/TimerProgressView.swift
+++ Uni/Source/Scene/Timer/Component/TimerProgressView.swift
@@ -1,68 +1,44 @@
-//
-//  TimerProgressView.swift
-//  Uni
-//
-//  Created by 홍유정 on 2023/09/30.
-//
-
-import AudioToolbox
 import SwiftUI
 import SDSKit
 
 struct TimerProgressView: View {
-
-    @EnvironmentObject var timerState: TimerState
-
-    let timer = Timer.publish(every: 1, on: .main, in: .common)
-        .autoconnect()
-
+    @ObservedObject var timerState: TimerData
     var body: some View {
         ZStack {
-            Circle()
-                .fill(Color.clear)
-                .frame(width: 339, height: 339)
-                .overlay(
-                    Circle().stroke(Color(uiColor: .gray150), lineWidth: 8)
-                )
-
+            ZStack {
+                Circle()
+                    .foregroundColor(Color(uiColor: .gray150))
+                    .frame(maxWidth: .infinity)
+                Circle()
+                    .foregroundColor(Color(uiColor: .gray000))
+                    .frame(maxWidth: .infinity - 16)
+            }
             Circle()
                 .fill(Color(uiColor: .gray000))
-                .frame(width: 339, height: 339)
+                .frame(maxWidth: .infinity)
                 .overlay(
                     Circle()
                         .trim(from: 0,
-                              to: CGFloat(timerState.timeRemaining)/CGFloat(timerState.totalSeconds)
+                              to: CGFloat(timerState.remainingTime)/CGFloat(timerState.totalTime)
                              )
-                        .stroke(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round
-                                                  )
+                        .stroke(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round)
                         )
-                        .foregroundColor(Color(uiColor: .lightBlue500))
+                        .foregroundColor(timerState.isTimerRunning ? Color(uiColor: .lightBlue500) : Color(uiColor: .gray150))
+                        .animation(.linear(duration: 1))
                 )
                 .rotationEffect(.degrees(-90))
-
             HStack {
-                Text(String(format: "%02d", timerState.timeRemaining/60))
+                Text(String(format: "%02d", timerState.remainingTime/60))
+                    .foregroundColor(Color(uiColor: .gray600))
                     .frame(width: 100)
                 Text(" : ")
-                Text(String(format: "%02d", timerState.timeRemaining%60))
+                    .foregroundColor(Color(uiColor: .gray600))
+                Text(String(format: "%02d", timerState.remainingTime%60))
+                    .foregroundColor(Color(uiColor: .gray600))
                     .frame(width: 100)
             }
             .font(.system(size: 68))
         }
-        .frame(height: 250)
-        .padding()
-        .onReceive(timer) { _ in
-//            withAnimation {
-                if timerState.isTimerRunning {
-                    if timerState.timeRemaining > 0 {
-                        timerState.timeRemaining -= 1
-                    } else if timerState.timeRemaining == 0 {
-                        AudioServicesPlaySystemSound(1005)
-                        timerState.startTimer = false
-                        timerState.showToast = true
-                    }
-                }
-//            }
-        }
+        .frame(maxWidth: .infinity)
     }
 }
--- Uni/Source/Scene/Timer/Component/TimerTipView.swift
+++ Uni/Source/Scene/Timer/Component/TimerTipView.swift
@@ -1,33 +1,25 @@
-//
-//  TimerTipView.swift
-//  Uni
-//
-//  Created by 홍유정 on 10/7/23.
-//
-
 import SwiftUI
 import SDSKit
 
 struct TimerTipView: View {
-
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             Text("타이머 활용법")
                 .font(Font(SDSFont.subTitle.font))
                 .foregroundStyle(Color(uiColor: .lightBlue500))
-                .padding(.vertical, 12)
+                .padding(.top, 12)
                 .padding(.leading, 12)
-
             Text("추천하는 제한 시간은 5분입니다\n타이머를 설정하고 원하는 시간 동안 승부를 즐겨보세요")
-                .font(Font(SDSFont.body2Long.font))
-                .foregroundStyle(Color(uiColor: .gray600))
-                .padding(.horizontal, 12)
-                .padding(.bottom, 12)
                 .lineLimit(2)
                 .lineSpacing(10.0)
+                .font(Font(SDSFont.body2Long.font))
+                .foregroundStyle(Color(uiColor: .gray600))
+                .padding(.top, 10)
+                .padding(.bottom, 12)
+                .padding(.horizontal, 12)
         }
+        .frame(maxWidth: .infinity)
         .background(Color(uiColor: .gray000))
         .cornerRadius(10.0)
-        .padding(.top, 32)
     }
 }
