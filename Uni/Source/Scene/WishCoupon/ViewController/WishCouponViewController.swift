ac218ba9e4be2074e5a7c34c660d0ab792fa7d05
ryuchanghwi <ckdgnl10@naver.com>
2023-12-10 18:23:05 +0900

    ✅[CHORE] : 소원권 목록 화면 텍스트 수정

--- Uni/Source/Scene/WishCoupon/ViewController/WishCouponViewController.swift
+++ Uni/Source/Scene/WishCoupon/ViewController/WishCouponViewController.swift
@@ -1,47 +1,28 @@
-//
-//  WishCouponViewController.swift
-//  Uni
-//
-//  Created by 김사랑 on 2023/07/16.
-//
-
 import UIKit
 import Then
 import SDSKit
 import CHTCollectionViewWaterfallLayout
 
 class WishCouponViewController: BaseViewController {
-    
-    // MARK: - Property
-    
-    
-    
     // MARK: - UI Property
-    
     private var wishCouponView = WishCouponView()
     private let wishCouponRepository = WishCouponRepository()
-    
     // MARK: - Life Cycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setStyle()
         setLayout()
         actions()
         setConfig()
         backButtonTapped()
     }
-    
     override func loadView() {
         super.loadView()
         wishCouponView = WishCouponView(frame: self.view.frame)
         self.view = wishCouponView
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        ///나
-        
+        // 나
         if let userId = UserDefaultsManager.shared.loadInt(.userId) {
             view.showIndicator()
             wishCouponRepository.getWishCouponData(userId: userId) { [weak self] data in
@@ -53,9 +34,7 @@ class WishCouponViewController: BaseViewController {
                 strongSelf.view.removeIndicator()
             }
         }
-        
-        
-        ///너
+        // 너
         if let partnerId = UserDefaultsManager.shared.loadInt(.partnerId) {
             view.showIndicator()
             wishCouponRepository.getWishCouponData(userId: partnerId) { [weak self] data in
@@ -67,42 +46,29 @@ class WishCouponViewController: BaseViewController {
             }
         }
     }
-    
     // MARK: - Setting
     internal override func setConfig() {
         self.wishCouponView.wishCouponCollectionView.delegate = self
         self.wishCouponView.wishCouponYourCollectionView.delegate = self
     }
-    
-    private func setStyle() {
-    }
-    
-    override func setLayout() {
-    }
-    
     // MARK: - Action Helper
-    
     private func actions() {
         wishCouponView.wishCouponCountView.myButton.addTarget(self, action: #selector(myButtonTapped), for: .touchUpInside)
         wishCouponView.wishCouponCountView.yourButton.addTarget(self, action: #selector(yourButtonTapped), for: .touchUpInside)
     }
-    
     @objc func myButtonTapped() {
         switchToMyWishCouponView(showMyWishCoupon: true)
         print("switchMyButton")
     }
-    
     @objc func yourButtonTapped() {
         switchToMyWishCouponView(showMyWishCoupon: false)
         print("switchYourButton")
     }
-    
     func backButtonTapped() {
         self.wishCouponView.navigationBar.backButtonCompletionHandler = { self.navigationController?.popViewController(animated: true)
             print("백버튼 클릭해찌!")
         }
     }
-    
     // MARK: - Custom Method
     private func switchToMyWishCouponView(showMyWishCoupon: Bool) {
         if showMyWishCoupon {
@@ -117,11 +83,10 @@ class WishCouponViewController: BaseViewController {
                 self.wishCouponView.wishCouponCountView.myButton.titleLabel?.font = SDSFont.subTitle.font
                 self.wishCouponView.wishCouponCollectionView.scrollToInitialPosition()
             }
-        }
-        else {
+        } else {
             /// 상대 소원권
             DispatchQueue.main.async {
-                var wishCouponYourData: WishCouponDataModel?
+//                var wishCouponYourData: WishCouponDataModel?
                 if let count = self.wishCouponView.wishCouponYourCollectionView.wishCouponYourData?.wishCouponList.count {
                     if count == 0 {
                         self.wishCouponView.wishCouponYourCollectionView.isHidden = true
@@ -131,7 +96,7 @@ class WishCouponViewController: BaseViewController {
                         self.wishCouponView.wishCouponEmptyView.isHidden = true
                     }
                 }
-                self.wishCouponView.wishCouponEmptyView.noneLabel.text = "아직 상대의 소원권이 없어요!"
+                self.wishCouponView.wishCouponEmptyView.noneLabel.text = "아직 상대의 소원권이 없어요"
                 self.wishCouponView.wishCouponCollectionView.isHidden = true
                 self.wishCouponView.wishCouponYourCollectionView.isHidden = false
                 self.wishCouponView.wishCouponCountView.myButton.setTitleColor(.gray300, for: .normal)
@@ -142,43 +107,36 @@ class WishCouponViewController: BaseViewController {
             }
         }
     }
-    
     func configureData(wishCouponData: WishCouponDataModel) {
         wishCouponView.wishCouponCountView.countLabel.text = "사용 가능한 소원권이 \(wishCouponData.availableWishCoupon ?? 0)개 있어요"
     }
 }
-
 extension WishCouponViewController: WishCouponSelectedCollectionView {
     func selectPartnerCouponId(couponId: Int) {
         let couponDetailVC = YourWishViewController()
         couponDetailVC.wishId = couponId
         self.navigationController?.pushViewController(couponDetailVC, animated: true)
     }
-    
     func selectCouponId(couponId: Int) {
         let couponDetailVC = MyWishViewController()
         couponDetailVC.wishId = couponId
         self.navigationController?.pushViewController(couponDetailVC, animated: true)
     }
-    
     func selectMakeCoupon() {
         let makeCouponVC = MakeWishViewController()
         makeCouponVC.makeWishCompletionHandler = {
             if let userid = UserDefaultsManager.shared.loadInt(.userId),
                let partnerId = UserDefaultsManager.shared.loadInt(.partnerId) {
-                
                 self.view.showIndicator()
                 self.wishCouponRepository.getWishCouponData(userId: userid) { [weak self] data in
                     guard let strongSelf = self else {return}
-                    
                     print(data)
                     strongSelf.configureData(wishCouponData: data)
                     strongSelf.wishCouponView.myWishCouponData = data
                     strongSelf.wishCouponView.wishCouponCollectionView.wishCouponCollectionView.reloadData()
                     strongSelf.view.removeIndicator()
                 }
-                
-                ///너
+                // 너
                 self.view.showIndicator()
                 self.wishCouponRepository.getWishCouponData(userId: partnerId) { [weak self] data in
                     guard let strongSelf = self else {return}
