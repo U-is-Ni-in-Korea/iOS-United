ca9fcf0c77ac1e5eb0da2aa7b2ea31215179552e
ryuchanghwi <ckdgnl10@naver.com>
2024-01-05 21:31:56 +0900

    ➕[ADD] : Domain 모듈 생성

---
+++ Domain/Domain.h
@@ -0,0 +1,18 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by 류창휘 on 1/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
